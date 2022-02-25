d8278fece49e09f8baf47cbe15b189f91085b067
langdten <51236478+tlangdun@users.noreply.github.com>
2022-02-25 11:00:33 +0100

    lol

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,21 +1,15 @@
-import logo from '../assets/logo.svg';
+import logo from '../assets/Capture.jpg';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+        <img src={logo}/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
           this is just a test.
         </p>
-        <a
-          className='text-blue-600'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
+
       </header>
     </div>
   );
