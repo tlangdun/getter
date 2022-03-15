2b430ed08fc895b19956eb51fcf1709d7a42c970
martinoswald <oswalma5@students.zhaw.ch>
2022-03-15 21:07:15 +0100

    adds report generation capabilities

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import app from './services/firebaseconfig';
+import { getAuth } from 'firebase/auth'
+
+function App() {
+  const auth = getAuth(app);
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>Currently under construction.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
