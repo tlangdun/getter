47def820868fde01ea2c409323b3fec686030170
martinoswald <oswalma5@students.zhaw.ch>
2022-03-15 21:31:50 +0100

    removes duplicate component

--- client/src/components/app/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders under construction', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/under construction/i);
-  expect(linkElement).toBeInTheDocument();
-});
