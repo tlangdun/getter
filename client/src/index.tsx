e894246121b96e0acd864432ee2019ebce740d4f
martinoswald <oswalma5@students.zhaw.ch>
2022-03-18 16:56:19 +0100

    adds redux to the project

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './store/store'
 
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}></Provider>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
