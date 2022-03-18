103a5d0869936561190d4599ae6642a1fb319597
martinoswald <oswalma5@students.zhaw.ch>
2022-03-18 17:00:28 +0100

    passes the <App> component as a child into store

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,14 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './store/store'
-
-
+import store from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}></Provider>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
