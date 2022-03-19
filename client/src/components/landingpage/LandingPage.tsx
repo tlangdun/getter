5ad7307bced8096a688a71f537293cbcf643f852
villiren <villiren@students.zhaw.ch>
2022-03-19 10:12:21 +0100

    LandingPage layout done

--- client/src/components/landingpage/LandingPage.tsx
+++ client/src/components/landingpage/LandingPage.tsx
@@ -1,5 +1,18 @@
+import FeaturesSection from "./FeaturesSection";
+import HeroSection from "./HeroSection";
+import NewsletterSection from "./NewsletterSection";
+import TeamSection from "./TeamSection";
+import FooterSection from "./FooterSection";
+
 const Landingpage = () => {
-    return ( <></> );
+    return ( <>
+        < HeroSection />
+        < FeaturesSection />
+        <NewsletterSection />
+        <TeamSection />
+        <FooterSection />
+
+    </> );
 }
 
 export default Landingpage;
