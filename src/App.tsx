13821567edad5bedbc9c01f7d553dabf46d85cb6
kleisval <kleisval@students.zhaw.ch>
2022-03-26 14:57:25 +0100

    removed client folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import app from './services/firebaseconfig';
 import { getAuth } from 'firebase/auth'
+import Dashboard from './routes/dashboard/Dashboard';
 
 function App() {
   const auth = getAuth(app);
   return (
     <div className='App'>
       <header className='App-header'>
-        <p>Currently under construction.</p>
+        <Dashboard />
       </header>
     </div>
   );
