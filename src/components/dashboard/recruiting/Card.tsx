f2a0f24b7a9a1ab945308753a83f5b31cd80fd9c
kleisval <kleisval@students.zhaw.ch>
2022-03-30 14:39:55 +0200

    added button to add candidate to candidate-list

--- src/components/dashboard/recruiting/Card.tsx
+++ src/components/dashboard/recruiting/Card.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { MailIcon, PhoneIcon } from '@heroicons/react/solid'
+import { MailIcon, PhoneIcon,PlusSmIcon } from '@heroicons/react/solid'
 type Person = {
     email:string;
     name:string;
@@ -11,12 +11,22 @@ type Person = {
 interface Props {
     person:Person;
 }
+function addToCandidatesList(id:string) {
+    alert("add " + id + "to candidate list")
+}
 const Card:FC<Props> = ({ person }) => {
   return(
     <li
         key={person.email}
-        className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
+        className="col-span-1 relative selection:flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
     >
+        <button
+        type="button"
+        className="inline-flex absolute right-0 p-1 border border-transparent shadow-sm text-white bg-violet-500 hover:bg-violet-700 focus:outline-none"
+        onClick={() => addToCandidatesList(person.email)}
+        >
+            <PlusSmIcon className="h-6 w-6" aria-hidden="true" />
+        </button>
         <div className="flex-1 flex flex-col p-8">
         <img className="w-32 h-32 flex-shrink-0 mx-auto rounded-full" src={person.imageUrl} alt="" />
         <h3 className="mt-6 text-gray-900 text-sm font-medium">{person.name}</h3>
