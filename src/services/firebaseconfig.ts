47d59f40f3f54313a93fa833854079c2e7309f51
martinoswald <oswalma5@students.zhaw.ch>
2022-04-05 17:14:35 +0200

    initializes db and auth

--- src/services/firebaseconfig.ts
+++ src/services/firebaseconfig.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -7,9 +9,13 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  measurementId: process.env.REACT_APP_MEASURMENT_ID
+  measurementId: process.env.REACT_APP_MEASURMENT_ID 
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
 
 export default app;
