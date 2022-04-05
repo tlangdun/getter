27b3f94ca8e28ad87f215a1e2e9169fdf5699d38
tlangdun <51236478+tlangdun@users.noreply.github.com>
2022-04-05 11:17:43 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import app from './services/firebaseconfig';
-import { getAuth } from 'firebase/auth'
+// import { getAuth } from 'firebase/auth'
 
 function App() {
-  const auth = getAuth(app);
+  //const auth = getAuth(app);
   return (
     <div className='App'>
       <header className='App-header'>
