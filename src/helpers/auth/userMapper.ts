e8f9f85c9b593f7f3df34d319e7fb876b59fa8b5
martinoswald <oswalma5@students.zhaw.ch>
2022-04-10 20:57:49 +0200

    maps firebase user

--- src/helpers/auth/userMapper.ts
+++ src/helpers/auth/userMapper.ts
@@ -1,20 +1,41 @@
 import { User } from 'firebase/auth';
-import { collection, query, where } from 'firebase/firestore';
+import { doc, getDoc } from 'firebase/firestore';
 import { db } from '../../services/firebaseconfig';
 import { GetterUser } from '../../store/Models';
 
-const userMapper = (user: User | null): GetterUser => {
+const userMapper = async (user: User | null): Promise<GetterUser> => {
   if (!user) {
     return null;
   }
-  
-  const userCollection = collection(db, 'Users')
-  console.log(userCollection)
 
-  return {
+  const docRef = doc(db, "Users", user.uid);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+   const u = docSnap.data()
+   return {
     uid: user.uid,
-    name: user.displayName,
+    address_postcode: u.address_postcode,
+    availability: u.availability,
+    birth_date: u.birth_date,
+    canton: u.canton,
+    city_of_residence: u.city_of_residence,
+    email: u.email,
+    job_role: u.job_role,
+    first_name: u.first_name,
+    last_name: u.last_name,
+    pic_url: u.pic_url,
+    short_bio: u.short_bio,
+    salary_range: {
+      start: u.salary_range.start,
+      end: u.salary_range.end
+    },
+    work_experience: u.work_experience
   };
+  } else {
+    console.log("No such document!");
+    return null;
+  }
 };
 
 export { userMapper };
--- src/store/Models.ts
+++ src/store/Models.ts
@@ -1,4 +1,27 @@
+type WorkExperience = {
+  description: string;
+  employer: string;
+  job_role: string;
+  end_date: string;
+  start_date: string;
+}
+
 export type GetterUser = null | {
     uid: string;
-    name: string | null;
+    address_postcode: string;
+    availability: string;
+    birth_date: string;
+    canton: string;
+    city_of_residence: string;
+    email: string;
+    job_role: string;
+    first_name: string;
+    last_name: string;
+    pic_url: string;
+    short_bio: string;
+    salary_range: {
+      start: number;
+      end: number;
+    }
+    work_experience: WorkExperience[];
   };
