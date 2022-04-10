12455e8d287d94e026e226152876ab752efa74fb
martinoswald <oswalma5@students.zhaw.ch>
2022-04-10 20:46:33 +0200

    adds Storage to FirebaseConfig

--- src/services/firebaseconfig.ts
+++ src/services/firebaseconfig.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -17,5 +18,7 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
 export const db = getFirestore(app);
+
+export const storage = getStorage(app);
 
 export default app;
