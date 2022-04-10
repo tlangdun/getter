2c2ee843eb07840ba6ba6f039f9bce33cfedb56e
martinoswald <oswalma5@students.zhaw.ch>
2022-04-10 21:01:50 +0200

    imports onAuthStateChanged hook

--- src/routes/auth/AuthRoute.tsx
+++ src/routes/auth/AuthRoute.tsx
@@ -1,3 +1,4 @@
+import { onAuthStateChanged } from 'firebase/auth';
 import { FC, useEffect } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import { userMapper } from '../../helpers/auth/userMapper';
