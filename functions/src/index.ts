471ab563a75b7dd13e48e820f63d8c967985b7c9
langdten <langdten@students.zhaw.ch>
2022-04-30 11:41:47 +0200

    added setrecruiter cloudfunction

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,20 @@
 import * as functions from "firebase-functions";
+const admin = require('firebase-admin');
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+exports.setRecruiter = functions.region('europe-west1').https.onCall((data, context) => {
+  if (context.auth) {
+    const uid = context.auth.uid;
+    return admin.auth.setCustomUserClaims(uid, {
+      recruiter: true
+    }).then(() => {
+      return {
+        message: 'Success! User has been set to Recruiter'
+      }
+    }).catch((err: any) =>{
+      return err;
+    })
+    }
 });
