384a02de6b9c39ec684bda4a2f157920dd223109
villiren <villiren@students.zhaw.ch>
2022-04-30 15:31:28 +0200

    Updated Landingpage

--- src/routes/landing/LandingPage.tsx
+++ src/routes/landing/LandingPage.tsx
@@ -1,8 +1,18 @@
 import { FC } from 'react';
+import HeroSection from "../../components/landingpage/HeroSection";
+import FeaturesSection from "../../components/landingpage/FeaturesSection";
+import NewsletterSection from "../../components/landingpage/NewsletterSection";
+import TeamSection from "../../components/landingpage/TeamSection";
+import FooterSection from "../../components/landingpage/FooterSection";
 
 const LandingPage: FC = () => {
   return(
-    <><div data-testid="landing-page"></div></>
+    <><div data-testid="landing-page"></div>
+    <HeroSection></HeroSection>
+    <FeaturesSection></FeaturesSection>
+    <NewsletterSection></NewsletterSection>
+    <TeamSection></TeamSection>
+    </>
   );
 };
 
