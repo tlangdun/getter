8122c8ca2345f6d6397281db693f4885e1d448d2
langdten <langdten@students.zhaw.ch>
2022-05-01 15:15:28 +0200

    added recruiterquerylist function

---
+++ src/queries/getRecruiterQueryList.ts
@@ -0,0 +1,31 @@
+import { User, getAuth, onAuthStateChanged } from 'firebase/auth';
+import { collection, CollectionReference, doc, getDoc, getDocs } from 'firebase/firestore';
+import { db } from '../services/firebaseconfig';
+
+// export async function getRecruiterQueryList(): Promise<string[]>{
+//     const res: string[] = []
+//     const auth = getAuth();
+//     onAuthStateChanged(auth, (user) => {
+//         if (user) {
+//           // User is signed in, see docs for a list of available properties
+//           // https://firebase.google.com/docs/reference/js/firebase.User
+//           const uid = user.uid;
+//           const queryRef = collection(db, `Recruiters/${uid}/Queries`);
+//           const names = getDocsFromCollection(queryRef)
+//           return names
+//         }
+//       });
+//       return res
+// }
+
+export async function getRecruiterQueryList(): Promise<string[]>{
+  const queryRef = collection(db, `Recruiters/mVnIMMnXAF9s3Mrs5UJs/Queries`);
+  const names = getDocsFromCollection(queryRef)
+  return names
+}
+async function getDocsFromCollection(queryRef: CollectionReference): Promise<string[]>{
+    const querySnapshot = await getDocs(queryRef)
+    const names = querySnapshot.docs.map(d => d.id)
+    console.log(names)
+    return names
+}
