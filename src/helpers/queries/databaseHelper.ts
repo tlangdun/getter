27094745ad66cee70581694552c97f0dd522d778
goodtho <Haloreach!!7>
2022-05-02 20:54:05 +0200

    ADDED: getDocumentsByFilter

--- src/helpers/queries/databaseHelper.ts
+++ src/helpers/queries/databaseHelper.ts
@@ -1,12 +1,9 @@
-import { User } from 'firebase/auth';
-import { collection, doc, getDoc, getDocs, setDoc } from 'firebase/firestore';
-import { db } from '../../services/firebaseconfig';
-import { access_level, GetterUser } from '../../store/models/userModel';
-import {user} from "firebase-functions/lib/providers/auth";
-import firebase from "firebase/compat";
-import {firestore} from "firebase-admin";
+import {collection, doc, getDoc, getDocs, setDoc, where, query} from 'firebase/firestore';
+import {db, storage} from '../../services/firebaseconfig';
+import {access_level, GetterUser} from '../../store/models/userModel';
+import {QueryFilter} from "../../store/models/queryModel";
 
-const getRecruiterByUserId = async (userId: string):Promise<GetterUser> => {
+export const getRecruiterByUserId = async (userId: string): Promise<GetterUser> => {
     const docRef = doc(db, `Users/${userId}`)
     const docSnap = await getDoc(docRef);
 
@@ -22,12 +19,12 @@ const getRecruiterByUserId = async (userId: string):Promise<GetterUser> => {
             short_bio: u.short_bio
         };
     } else {
-    console.log(`No User By UserId: ${userId}!`);
+        console.log(`No User By UserId: ${userId}!`);
         return null;
     }
 }
 
-const getTalentByUserId = async (userId: string):Promise<GetterUser> => {
+export const getTalentByUserId = async (userId: string): Promise<GetterUser> => {
     const docRef = doc(db, `Users/${userId}`);
     const docSnap = await getDoc(docRef);
 
@@ -52,7 +49,6 @@ const getTalentByUserId = async (userId: string):Promise<GetterUser> => {
 
     if (docSnap.exists()) {
         const u = docSnap.data();
-        console.log('DOCSNAP.DATA')
         console.log(docSnap.data())
         return {
             uid: userId,
@@ -82,12 +78,72 @@ const getTalentByUserId = async (userId: string):Promise<GetterUser> => {
     }
 }
 
-const updateRecruiterByUserId = async (recruiterForm: GetterUser): Promise<boolean> => {
-    if(!recruiterForm)return false;
+// ToDo: ToDo :^)
+export const updateRecruiterByUserId = async (recruiterForm: GetterUser): Promise<boolean> => {
+    if (!recruiterForm) return false;
 
     const docRef = doc(db, `Users/${recruiterForm?.uid}`);
     const docSnap = await setDoc(docRef, recruiterForm);
     return true;
 }
 
-export {getTalentByUserId, getRecruiterByUserId};
+export const getDocumentsByFilter = async (recruiterForm: QueryFilter): Promise<GetterUser[]> => {
+    const ref = collection(db, 'Users');
+
+    const queryFilters = []
+
+    if (recruiterForm.availability != null)
+        queryFilters.push(where('availability', '>=', recruiterForm.availability),)
+
+    if (recruiterForm.canton != null)
+        queryFilters.push(where('canton', '==', recruiterForm.canton),)
+
+    if (recruiterForm.job_role != null)
+        queryFilters.push(where('job_role', '==', recruiterForm.job_role),)
+
+    if (recruiterForm.skills != null)
+        queryFilters.push(where('skills', 'array-contains-any', recruiterForm.skills),)
+
+    if (recruiterForm.programming_languages != null)
+        queryFilters.push(where('programming_languages', 'array-contains-any', recruiterForm.programming_languages),)
+
+    if (recruiterForm.spoken_languages != null)
+        queryFilters.push(where('spoken_languages', 'array-contains-any', recruiterForm.spoken_languages),)
+
+    if (recruiterForm.work_experience != null)
+        queryFilters.push(where('work_experience', '>=', recruiterForm.work_experience),)
+
+
+    const qResult = query(ref, ...queryFilters);
+
+    const returnList: Array<GetterUser> = [];
+
+    return getDocs(qResult).then(result => {
+        result.forEach(r => {
+            const u = r.data();
+            returnList.push({
+                uid: u.userId,
+                access_level: u.access_level,
+                email: u.email,
+                first_name: u.first_name,
+                last_name: u.last_name,
+                pic_url: u.pic_url,
+                short_bio: u.short_bio,
+                address_postcode: u.address_postcode,
+                availability: u.availability,
+                birth_date: u.birth_date,
+                canton: u.canton,
+                city_of_residence: u.city_of_residence,
+                job_role: u.job_role,
+                skills: u.skills,
+                programming_languages: u.programmmingLanguages,
+                salary_range: {
+                    start: u.salary_range.start,
+                    end: u.salary_range.end,
+                },
+                work_experience: u.work_experience,
+            })
+        })
+        return returnList;
+    });
+}
