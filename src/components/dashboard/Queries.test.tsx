c0f5442fae9ebc5f6d11c2653efe9c260cd4274e
iamjhp <jooho.park@outlook.com>
2022-05-05 19:54:23 +0200

    test

--- src/components/dashboard/Queries.test.tsx
+++ src/components/dashboard/Queries.test.tsx
@@ -28,4 +28,23 @@ describe('Queries page', () => {
 
     expect(document.getElementById("pop-up")).toBeInTheDocument();
   })
+
+  test('Shows the correct number of queries buttons', () => {
+    render(
+      <QueriesModul.QueryItem names={testQueries}/>
+    );
+    const items = screen.getAllByTestId("query-button")
+    expect(items.length).toBe(3)
+    
+  })
+
+  test('Should display the correct query name', () => {
+    render(
+      <QueriesModul.QueryItem names={testQueries}/>
+    );
+    const queryName = screen.getByRole('button', { 
+      name: /Senior Data Scientist/i 
+    })
+    expect(queryName).toHaveTextContent('Senior Data Scientist')
+  })
 })
