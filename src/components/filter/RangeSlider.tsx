1f77bdaf26248ec8009003ff1b73f29ad229088d
kleisval <kleisval@students.zhaw.ch>
2022-05-12 09:18:41 +0200

    updated rangeslider component

--- src/components/filter/RangeSlider.tsx
+++ src/components/filter/RangeSlider.tsx
@@ -2,16 +2,20 @@ import { FC } from 'react';
 
 interface Props {
     event:any;
+    id:string;
     value:string;
     name:string;
+    min:string;
+    max:string;
+    step:string;
 }
 
 const RangeSlider:FC<Props> = (props) => {
   return(
-    <fieldset className="w-1/2 p-2">
+    <fieldset className="p-2">
         <legend className="block font-medium">{props.name}</legend>
-        <label htmlFor="availability-range" className="min-w-0 flex-1 text-gray-600">{props.value} %</label>
-        <input id="availability-range" onClick={props.event} type="range" min="0" step="5" max="100" className="w-full h-3 accent-violet-600" />
+        <label htmlFor={props.id} className="min-w-0 flex-1 text-gray-600">{props.value}</label>
+        <input id={props.id} onClick={props.event} type="range" min={props.min} step={props.step} max={props.max} className="w-full h-3 accent-violet-600" />
     </fieldset>   
   );
 };
