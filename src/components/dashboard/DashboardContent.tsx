f60012ab3d10f774a7438d0a58ff61240e6993c8
kleisval <kleisval@students.zhaw.ch>
2022-05-12 15:25:06 +0200

    added default dashboard content

--- src/components/dashboard/DashboardContent.tsx
+++ src/components/dashboard/DashboardContent.tsx
@@ -1,7 +1,61 @@
 import { FC } from 'react';
+import ContentCard from './defaultpage/ContentCard';
+import FullCard from './defaultpage/FullCard';
+let fullCardContent = {
+  title: "Recruiting Dashboard",
+  text: "Filter through our database and find matching talents. Save your queries for future searches and chat with the talents in the messages tab."
+}
+let contentCard1 = {
+  title:"Recruiting",
+  text:"Filter through all candidates and find candidates that suits the needs of your company / client.",
+  image:"https://firebasestorage.googleapis.com/v0/b/getter-38760.appspot.com/o/getter%2Frecruiting-pexels-gustavo-fring-3874035.jpg?alt=media&token=37d1f285-acb7-4d0f-b06a-0ba5a1e99521",
+  link:"/dashboard/recruiting"
+}
+let contentCard2 = {
+  title:"Queries",
+  text:"Save and edit your queries for easier access in your future recruiting process.",
+  image:"https://firebasestorage.googleapis.com/v0/b/getter-38760.appspot.com/o/getter%2Fqueries-pexels-pixabay-356079.jpg?alt=media&token=4c10794f-a07c-4cf9-8cca-f27ad28e5c0b",
+  link:"/dashboard/queries"
+}
+let contentCard3 = {
+  title:"Messages",
+  text:"Start a new conversation with a candidate over the recruiting page. Write with the Candidates in the messages tab.",
+  image:"https://firebasestorage.googleapis.com/v0/b/getter-38760.appspot.com/o/getter%2Fmessages-pexels-pixabay-261763.jpg?alt=media&token=53ba4e1c-e67c-40c4-b1ea-ed19c32c802a",
+  link:"/dashboard/messages"
+}
 const DashboardContent:FC = () => {
   return(
-    <><div data-testid="dashboard">Default Dashboard</div></>
+    <>
+    <div data-testid="dashboard">
+      <FullCard title={fullCardContent.title} text={fullCardContent.text}/>
+      <div className="grid grid-flow-col mb-4">
+          <div className="col-span-1 rounded m-2">
+            <ContentCard 
+            title={contentCard1.title}
+            text={contentCard1.text}
+            link={contentCard1.link} 
+            image={contentCard1.image}
+            />
+          </div>
+          <div className="col-span-1 rounded m-2">
+            <ContentCard 
+            title={contentCard2.title}
+            text={contentCard2.text}
+            link={contentCard2.link} 
+            image={contentCard2.image}
+            />
+          </div>
+          <div className="col-span-1 rounded m-2">
+            <ContentCard 
+            title={contentCard3.title}
+            text={contentCard3.text}
+            link={contentCard3.link} 
+            image={contentCard3.image}
+            />
+          </div>      
+        </div>
+    </div>
+    </>
   );
 };
 
---
+++ src/components/dashboard/defaultpage/ContentCard.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+interface Props {
+    title:string;
+    text:string;
+    link:string;
+    image:string;
+}
+const ContentCard:FC<Props> = (props) => {
+  return(
+    <>
+        <Link to={props.link}>
+        <div className="relative h-full max-w-sm rounded overflow-hidden shadow-lg">
+            <div className='absolute inset-0'>
+                <img className="h-full w-full object-cover" src={props.image} alt="Sunset in the mountains"/>
+                <div className="absolute inset-0 bg-gray-400 mix-blend-multiply" />
+            </div>
+            <div className='relative'>
+                <div className="px-6 py-4">
+                    <div className="font-bold text-white text-xl mb-2">{props.title}</div>
+                    <p className="text-white text-base">
+                        {props.text}
+                    </p>
+                </div>
+            </div> 
+        </div>
+        </Link>
+    </>
+  );
+};
+
+export default ContentCard
