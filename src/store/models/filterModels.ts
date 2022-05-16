1b8012af26911500c5bacb01d371ea29e7155dc5
langdten <51236478+tlangdun@users.noreply.github.com>
2022-05-16 23:40:20 +0200

    fixed eslint

--- src/store/models/filterModels.ts
+++ src/store/models/filterModels.ts
@@ -1,5 +1,5 @@
-export let EmptyFilters = {
-    skills: [],
+export let EmptyFilters: Filters= {
+    skills: new Array(),
     programmingLanguages: [],
     jobRoles: [],
     countries: [],
@@ -9,11 +9,11 @@ export let EmptyFilters = {
 }
 
 export type Filters= {
-    skills: string[],
-    programmingLanguages:string[],
-    jobRoles: string[],
-    countries: string[],
-    region: string[],
-    spokenLanguages: string[],
+    skills: {value: string, label: string, checked: boolean}[],
+    programmingLanguages:{ value: string, label: string, checked: boolean }[],
+    jobRoles: { value: string, label: string, checked: boolean }[],
+    countries: { value: string, label: string, checked: boolean }[],
+    region: { value: string, label: string, checked: boolean }[],
+    spokenLanguages: { value: string, label: string, checked: boolean }[],
     work_experience: string[]
 }
