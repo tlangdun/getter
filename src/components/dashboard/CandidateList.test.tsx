a527ef7ee416df3f17f60f059b2f120e570a381d
Valentin Kleisner <valentin.kleisner@gmail.com>
2022-05-21 23:34:52 +0200

    removed code smells

--- src/components/dashboard/CandidateList.test.tsx
+++ src/components/dashboard/CandidateList.test.tsx
@@ -1,4 +1,4 @@
-import {fireEvent, render, screen, waitFor} from '@testing-library/react'
+import {fireEvent, render, screen} from '@testing-library/react'
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 import store from '../../store/store'
