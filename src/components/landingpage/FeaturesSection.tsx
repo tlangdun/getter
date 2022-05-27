f06bd44585bb85052b2881fcd4fcf3d742317541
villiren <villiren@students.zhaw.ch>
2022-05-27 14:58:25 +0200

    How To Section added

--- src/components/landingpage/FeaturesSection.tsx
+++ src/components/landingpage/FeaturesSection.tsx
@@ -14,7 +14,7 @@ const features = [
 
 const FeaturesSection = () => {
     return ( <>
-        <div className="bg-white">
+        <div className="bg-gray-50">
             <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
                 <div>
                     <h2 className="text-base font-semibold text-indigo-600 uppercase tracking-wide">Everything you need</h2>
